abfad7867e20a387aaf2b02cd503217e571bf054
YukiYasuno <85671824+C0A21130@users.noreply.github.com>
2026-01-03 00:02:17 +0900

    Add scoring benchmark configuration and implement mint and transfer workloads

--- README.md
+++ README.md
@@ -1,42 +1,23 @@
-# Hyperledger Caliper Benchmarks
+# Hyperledger Caliper ベンチマーク
 
-This repository contains sample benchmarks that may be used by Caliper, a blockchain performance benchmark framework. For more information on Caliper, please see the [Caliper main repository](https://github.com/hyperledger/caliper/)
+このリポジトリには、ブロックチェーンパフォーマンスベンチマークフレームワークであるhyperledger Caliperで使用できるサンプルベンチマークが含まれています。
+[Caliperメインリポジトリ](https://github.com/hyperledger/caliper/)をフォークし、信用スコアに基づく独自のNFTのスマートコントラクトのベンチマークを行っています。
 
-Associated performance reports, based on running these benchmarks, are published to the [repository github pages](https://hyperledger.github.io/caliper-benchmarks/).
+これらのベンチマークの実行に基づくパフォーマンスレポートは、[リポジトリのGitHub Pages](https://hyperledger.github.io/caliper-benchmarks/)に公開されています。
 
-## Repository Branches
+## mainブランチの内容
+mainブランチに含まれるベンチマークは、3つのディレクトリに分かれています：
 
-This repository has three branches:
+1. **benchmarks**. テスト設定とコールバックファイルで構成されています。テスト設定ファイルは、ベンチマークテストのパラメータを記述し、ベンチマーク中にCaliperクライアントによって実行されるコールバックファイルを参照します。Benchmarkフォルダには以下のサブフォルダが含まれます：
+    - **api** 単一のターゲットブロックチェーンのAPIに向けたテスト。
+	- **samples** ターゲットブロックチェーンプラットフォームが提供するネイティブサンプルに向けたテスト。
+	- **scenario** すべての（サポートされている）ターゲットブロックチェーンプラットフォームに有効な汎用シナリオ
+		- **scoring** 信用スコアを活用した認可システムを組み合わせたERC-721のスマートコントラクトを発行・転送する
+2. **networks**. ベンチマーク目的でテスト対象システムとして使用できるサンプルブロックチェーンネットワークで構成されています。
+3. **src**. SUTにデプロイされ、benchmarksフォルダ内のテストコールバックを介して対話されるソーススマートコントラクトファイルで構成されています。各スマートコントラクトは、対応するブロックチェーン技術の下にある独自のフォルダに保持されています。
 
-1. **main**. Contains sample benchmarks
-2. **reports**. Contains md files that are built and published to the `gh-pages` branch
-3. **gh-pages**. Contains the build output from the `reports` branch 
+## ベンチマークの実行
+このリポジトリに含まれるベンチマークを実行するには、すべてのファイルの使用を想定している[Hyperledger Caliper](https://github.com/hyperledger/caliper/)をインストールする必要があります。
 
-## Main Branch Contents
+GoQuorumネットワークでこのリポジトリを使用する詳細については、[GoQuorumネットワーク](/networks/quorum/4node/README.md)をご覧ください
 
-The benchmarks contained within the master branch are split into three directories:
-
-1. **benchmarks**. Comprises the test configuration and callback files. The test configuration files describe the benchmark test parameters and also reference the callback files that are executed by Caliper clients during the benchmark. The Benchmark folder contains the following subfolders:
-    - **api** Tests directed towards the API of a single target blockchain.
-	- **samples** Tests directed towards the native samples provided by target blockchain platforms.
-	- **scenario** Generic scenarios that are valid for all (supported) target blockchain platforms
-2. **networks**. Comprises sample blockchain networks that may be used as target systems under test (SUT) for benchmarking purposes.
-3. **src**. Comprises the source smart contract files that are deployed to the SUT and interacted with via the test callbacks located within the benchmarks folder. Each smart contract is held within its own folder, under the blockchain technology that the smart contract corresponds to.
-
-## Running a Benchmark
-
-To run any of the benchmarks present in this repository, it is required to have installed [Hyperledger Caliper]((https://github.com/hyperledger/caliper/)), which is the intended consumer of all the contained files.
-
-In general the steps are:
-
-1. Install the Caliper CLI - for details please see the [Caliper main repository](https://github.com/hyperledger/caliper/)
-2. Clone this repository
-3. Run a Caliper CLI command that targets one of the contained benchmarks.
-
-For detailed information about using this repo with hyperledger fabric see [Hyperledger Fabric Networks](./networks/fabric/README.md)
-
-For detailed information about using this repo with hyperledger besu see [Hyperledger Besu Networks](./networks/besu/1node-clique/README.md)
-
-## Extending the Documented Reports
-
-The documented reports are built automatically from the `reports` branch of this repository and subsequently hosted on the `gh-pages` branch; pull requests must be target the [`reports` branch](https://github.com/hyperledger/caliper-benchmarks/tree/reports) in order for any modifications to be built.
---
+++ src/ethereum/scoring/SsdlabToken.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.28;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./scoring/Scoring.sol";
+
+contract SsdlabToken is ERC721, AccessControl, Scoring {
+    // 状態変数の定義
+    uint256 private _nextTokenId = 0;
+    mapping(uint256 => string) private _tokenNames;
+    mapping(address => uint8) private _receiveCount;
+    mapping(uint256 => string) private _tokenURIs;
+
+    // イベントの定義
+    event Receive(string);
+    event Fallback(string);
+
+    constructor(address agent) ERC721("SsdlabToken", "SSDL") Scoring(agent) {
+        _grantRole(DEFAULT_ADMIN_ROLE, agent);
+    }
+
+    /// トークンを発行する関数
+    /// @param to トークンを受け取るアドレス
+    /// @param _tokenName トークン名
+    /// @return tokenId ミントされたトークンのID
+    function safeMint(address to, string memory _tokenName) public returns (uint256) {
+        uint256 tokenId = _nextTokenId;
+        _safeMint(to, tokenId);
+        setTokenName(tokenId, _tokenName);
+        _nextTokenId++;
+        return tokenId;
+    }
+
+    /// IPFS URI付きでトークンを発行する関数
+    /// @param to トークンを受け取るアドレス
+    /// @param _tokenName トークン名
+    /// @param _tokenURI トークンのIPFS URI
+    /// @return tokenId ミントされたトークンのID
+    function safeMintIPFS(address to, string memory _tokenName, string memory _tokenURI) public returns (uint256) {
+        uint256 tokenId = _nextTokenId;
+        _safeMint(to, tokenId);
+        setTokenName(tokenId, _tokenName);
+        setTokenURI(tokenId, _tokenURI);
+        _nextTokenId++;
+        return tokenId;
+    }
+    
+    /// トークンを転送する関数
+    /// @param from 送信元アドレス
+    /// @param to 送信先アドレス
+    /// @param tokenId 転送するトークンのID
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+        // アクセス制御の確認
+        // bool allowed = accessControl(from, to);
+        // require(allowed, "Transfer not allowed due to scoring rules");
+
+        // NFTの交換と取引履歴の更新
+        super.transferFrom(from, to, tokenId);
+        addEdge(from, to);
+    }
+
+    /// トークン名を設定する関数
+    /// @param tokenId トークンのID
+    /// @param tokenName トークン名
+    function setTokenName(uint256 tokenId, string memory tokenName) public {
+        _tokenNames[tokenId] = tokenName;
+    }
+
+    //// トークン名を取得する関数
+    /// @param tokenId トークンのID
+    /// @return tokenName トークン名
+    function getTokenName(uint256 tokenId) public view returns (string memory) {
+        return _tokenNames[tokenId];
+    }
+
+    /// トークンURLを設定する関数
+    /// @param tokenId トークンのID
+    /// @param tokenUrl トークンURL
+    function setTokenURI(uint256 tokenId, string memory tokenUrl) public {
+        _tokenURIs[tokenId] = tokenUrl;
+    }
+
+    /// トークンURIを取得する関数
+    /// @param tokenId トークンのID
+    /// @return tokenUrl トークンURL
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        return _tokenURIs[tokenId];
+    }
+
+    /// 指定したアドレスに0.1 Etherを送信する関数
+    /// @param _to Etherを受け取るアドレス
+    function faucet(address _to) public payable {
+        // check: パラメータや残高の確認
+        require(_to != address(0), "Invalid address");
+        require(address(this).balance > 0.3 ether, "Insufficient balance");
+        require(_to.balance < 0.1 ether, "Recipient has sufficient balance");
+
+        // effects: 状態変数である受信カウントの更新
+        _receiveCount[_to] += 1;
+
+        // interactions: _toへEtherの送信
+        (bool success, ) = _to.call{value: 0.1 ether}("");
+        require(success, "Transfer failed");
+    }
+
+    /// @dev See {ERC165-supportsInterface}, {AccessControl-supportsInterface}, {Scoring-supportsInterface}.
+    /// @param interfaceId インターフェースのID
+    /// @return isSupported インターフェースがサポートされているかどうかを確認する
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl, TrustRating)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // Ether受信時の処理
+    receive() external payable {
+        emit Receive("Received called");
+    }
+
+    // フォールバック関数
+    fallback() external payable {
+        emit Fallback("Fallback called");
+    }
+}
